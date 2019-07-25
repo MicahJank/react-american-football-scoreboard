a36e206287290f23687414d9af298da8b4243d44
MicahJank <MicahJank@users.noreply.github.com>
2019-07-25 12:52:37 -0500

    installed the styled-components for styling

--- src/components/awayButtons.js
+++ src/components/awayButtons.js
@@ -5,7 +5,7 @@ import "../App.css";
 function AwayButtons(props) {
     return (
         <>
-          <button onClick={() => props.setScore(props.score + (7+1))} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={() => props.setScore(props.score + 7)} className="awayButtons__touchdown">Away Touchdown</button>
           <button onClick={() => props.setScore(props.score + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </>
     );
--- src/components/homeButtons.js
+++ src/components/homeButtons.js
@@ -3,9 +3,10 @@ import {useState} from "react";
 import "../App.css";
 
 function HomeButtons(props) {
+
     return (
         <>
-        <button onClick={() => props.setScore(props.score + (7+1))} className="homeButtons__touchdown">Home Touchdown</button>
+        <button onClick={() => props.setScore(props.score + 7)} className="homeButtons__touchdown">Home Touchdown</button>
         <button onClick={() => props.setScore(props.score + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </>
     );
